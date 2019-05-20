189efba422130686ab36746b7a9ee07e4e064bc4
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2019-05-20 17:30:34 +0500

    added sequelized and defined porduct model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'hmz46w46w'
+const sequelize = new Sequelize('node-complete', 'root', 'hmz46w46w', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
