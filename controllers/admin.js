53348577db204ef5f5c402abfe6abbdc6fc4e6fc
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2019-05-20 17:14:03 +0500

    section 11 - learning sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,9 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save()
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
+  product
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -12,20 +12,19 @@ module.exports = class Product {
   }
 
   save() {
-    return db.execute("INSERT INTO products (title,price,imageUrl,description) VALUES(?, ?, ?, ?)",
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
       [this.title, this.price, this.imageUrl, this.description]
     );
   }
 
-  static deleteById(id) {
+  static deleteById(id) {}
 
-  }
-
-  static fetchAll(cb) {
+  static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
   static findById(id) {
-
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };
