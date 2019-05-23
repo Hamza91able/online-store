314d52e8c98037d2fba3e329fff6a469c5249fe8
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2019-05-23 18:45:35 +0500

    Created base model cart and added associations

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -35,9 +37,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsTo(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-    .sync()
+    .sync({ force: true })
     .then(result => {
         // console.log(result);
         return User.findByPk(1);
