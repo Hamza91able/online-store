26aedfb3ecf09cabdd95f88755e04fbbb627215a
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2019-05-26 18:42:39 +0500

    Configured database.js with mongodb and started ignoring database.js for security reasons

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'root', 'hmz46w46w', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
