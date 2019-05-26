7767710c948fad3c198e66f9d84fc330e1a2b05e
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2019-05-26 17:40:43 +0500

    Added basic adding of orders from cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -112,7 +111,17 @@ exports.postOrder = (req, res, next) => {
       return cart.getProducts();
     })
     .then(products => {
-      
+      return req.user.createOrder()
+        .then(order => {
+          return order.addProducts(products.map(product => {
+            product.orderItem = { quantity: product.cartItem.quantity };
+            return product;
+          }));
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result => {
+      res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
